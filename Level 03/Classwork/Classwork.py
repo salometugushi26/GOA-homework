1135cb7686f7d90a417c81f90cd761273f1d8884
salometugushi26 <sstugushi@gmail.com>
2025-02-16 22:49:38 +0400

    commit

---
+++ Level 03/Classwork/Classwork.py
@@ -0,0 +1,21 @@
+num1 = int(input("შეიყვანეთ პირველი რიცხვი: "))
+num2 = int(input("შეიყვანეთ მეორე რიცხვი: "))
+num3 = int(input("შეიყვანეთ მესამე რიცხვი: "))
+
+total = num1 + num2 + num3
+
+print(total)
+
+
+name = input("შეიყვანეთ თქვენი სახელი: ")
+print(name)
+
+#3
+
+num1 = int(input("შეიყვანეთ პირველი რიცხვი: "))
+num2 = int(input("შეიყვანეთ მეორე რიცხვი: "))
+
+if num1 > num2:
+    print(True)
+if num2 > num1:
+    print(False)
