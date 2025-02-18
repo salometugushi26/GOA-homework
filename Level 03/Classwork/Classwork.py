3fc31203f42774a708d9deba5611ca481be83368
salometugushi26 <sstugushi@gmail.com>
2025-02-18 18:03:03 +0400

    commit

--- Level 03/Classwork/Classwork.py
+++ Level 03/Classwork/Classwork.py
@@ -19,3 +19,15 @@ if num1 > num2:
     print(True)
 if num2 > num1:
     print(False)
+
+#4
+
+text = input("შეიყვანეთ სიტყვა: ")
+n = int(input("შეიყვანეთ რიცხვი: "))
+
+#5
+
+num1 = input("შეიყვანეთ პირველი რიცხვი: ")
+num2 = input("შეიყვანეთ მეორე რიცხვი: ")
+both = int(num1 + num2) + 1 
+print(both)
